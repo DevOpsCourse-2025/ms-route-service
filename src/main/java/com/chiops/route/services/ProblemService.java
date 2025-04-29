6d51f936de443be62eea9ad29699aecce3e2cc31
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-04-29 02:49:43 -0600

    feat: :sparkles: add functionality to register problems in routes

---
+++ src/main/java/com/chiops/route/services/ProblemService.java
@@ -0,0 +1,12 @@
+package com.chiops.route.services;
+
+import com.chiops.route.entities.Problem;
+import com.chiops.route.libs.dtos.ProblemDTO;
+
+
+public interface ProblemService {
+
+    ProblemDTO assignProblem(ProblemDTO problem);
+
+    ProblemDTO getProblemById(String vin);
+}
